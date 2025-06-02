8ff3549134b0720a7c0818fa2b9c30a8679dfcad
hdktsaam <hannes.dekesel@tsaam.be>
2025-06-02 11:29:54 +0200

    update woef.print

--- opdracht3.js
+++ opdracht3.js
@@ -5,6 +5,6 @@ const dier = require("./huisdier");
 
 const woef = dier.Huisdier("hond", "woef", 3);
 
-console.log(woef);
+console.log(woef.print());
 // resutlaat
 // > mijn hond woef is 3 jaar oud
